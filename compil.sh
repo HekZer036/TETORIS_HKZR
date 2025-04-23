2d90936f81ff45dd3f5671dccfa160e7650e5a36
HekZer036 <160788533+HekZer036@users.noreply.github.com>
2025-04-23 08:24:39 +0200

    Added Repaired C and sh file
    No changes in code - just a reupload

---
+++ compil.sh
@@ -0,0 +1,5 @@
+gcc -o tetoris Prod.c -lSDL2 -lSDL2_ttf -lSDL2_mixer -lSDL2_image
+
+./tetoris
+
+# for msys2 on windows 'gcc -o Tetoris.exe Prod.c -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -lSDL2_image'
